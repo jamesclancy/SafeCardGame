bb23045e878b68b0bbc7aafddd5f09d4f4e9be75
Clancy <clancysphone@gmail.com>
2021-07-31 08:56:03 -0400

    Update start-app.ps1

--- start-app.ps1
+++ start-app.ps1
@@ -1,3 +1,3 @@
-#docker run -d --name dev-postgres -e POSTGRES_PASSWORD=trident1814 -v c:/data/safecardgamepostgresqldata/:/var/lib/postgresql/data -p 5432:5432 postgres
+#docker run -d --name dev-postgres -e POSTGRES_PASSWORD=whatever-you-config-in-your-app-secrets -v c:/data/safecardgamepostgresqldata/:/var/lib/postgresql/data -p 5432:5432 postgres 
 docker start dev-postgres
 dotnet fake build --target run
